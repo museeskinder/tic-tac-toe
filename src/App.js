6057538f1698d632149ce1a47d117465d73d189b
museeskinder <eskindermuse@gmail.com>
2024-04-22 15:03:56 +0300

    winner checker

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function Board() {
 
   function handleClick(i) {
    const nextSquares = squares.slice(); 
-   if(squares[i])
+   if(squares[i] || calculateWinner(squares))
       return;
    if(xIsNext)
       nextSquares[i] = 'X';
@@ -19,6 +19,26 @@ export default function Board() {
       nextSquares[i] = 'O';
    setSquares(nextSquares);
    setXisNext(!xIsNext);
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [1, 4, 7]
+    ];
+    for(let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if(squares[a] === squares[b] && squares[a] === squares[c])
+        return squares[a];
+    }
+    return null;
   }
 
   return <>
---
+++ src/some.js
@@ -0,0 +1,5 @@
+const some = [3, 4, 4]
+
+if(some[0] === some[1] && some[0] === some[2])
+    console.log("all same")
+
